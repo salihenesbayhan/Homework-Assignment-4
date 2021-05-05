1f522b0c2d3e287d170d1b2745376f6b47fa4212
salihenesbayhan <80679472+salihenesbayhan@users.noreply.github.com>
2021-05-05 19:23:22 +0300

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,19 @@
+a= float (input("What is your birth year: "))
+if a <= 1900:
+    print(" You are a Lost Generation")
+elif a<=1927:
+    print(" You are a Greatest Generation")
+elif a<=1927:
+    print(" You are a Greatest Generation")
+elif a<=1945:
+    print(" You are a Silent Generation")
+elif a<=1964:
+    print(" You are a Baby boomers")
+elif a<=1980:
+    print(" You are a Generation X")
+elif a<=1996:
+    print(" You are a Millennials Generation")
+elif a<=2012:
+    print(" You are a Generation Z ")
+else:
+    print(" You are a Generation Alpha")
